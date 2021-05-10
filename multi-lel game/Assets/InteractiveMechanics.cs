16366c3cf8663583433c05d47430b17f43eff881
betaterra <b3tat3rra@gmail.com>
2021-05-10 14:52:00 -0700

    Complete

---
+++ multi-lel game/Assets/InteractiveMechanics.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveMechanics : MonoBehaviour
+{
+
+   private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("Hit Something!");
+    }
+
+
+}
---
+++ multi-lel game/Assets/MoveTo1stLvel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveTo1stLvel : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+        }
+    }
+
+
+}
+
